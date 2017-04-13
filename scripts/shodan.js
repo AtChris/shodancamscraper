b1be77bfc60d75fcd9950446c43c519b8cce529a
AtChris <christopherhage@gmail.com>
2017-04-12 21:50:04 -0400

    Shodan API request, displaying on screen, first taste of Jquery

--- scripts/shodan.js
+++ scripts/shodan.js
@@ -5,4 +5,31 @@ $(document).ready(function () {
         .done(function (data) {
             API_KEY = data;
         });
+
+    $("#searchButton").click(function () {
+        var searchValue = $("#searchBox").val();
+        searchShodan(searchValue);
+    });
+
+
+
+
+    function searchShodan(queryVal) {
+        $.get("https://api.shodan.io/shodan/host/search", {
+                key: API_KEY,
+                query: queryVal
+            })
+            .done(function (data) {
+                //console.log(data.matches);
+                var html = "";
+
+                data.matches.forEach(function (match) {
+                    html += (match.product || match.http.server) + "<br>";
+                });
+
+                $("#results").html(html);
+
+            });
+    }
+
 });
