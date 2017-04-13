bc1f620696bafbbb20a868e96338099977ad2ee6
atjeff <ownerejeff@gmail.com>
2017-04-12 22:03:47 -0400

    Peer programming on api call, bootstrap styling

--- scripts/shodan.js
+++ scripts/shodan.js
@@ -1,35 +1,33 @@
 $(document).ready(function () {
-    /* Get API KEY for shodan Api */
-    let API_KEY = "";
+    /* Get API KEY for Shodan Api */
+    let API_KEY = '';
     $.ajax('../key.txt')
         .done(function (data) {
             API_KEY = data;
         });
 
-    $("#searchButton").click(function () {
-        var searchValue = $("#searchBox").val();
+    $('#searchButton').click(function () {
+        var searchValue = $('#searchBox').val();
         searchShodan(searchValue);
     });
 
-
-
-
     function searchShodan(queryVal) {
-        $.get("https://api.shodan.io/shodan/host/search", {
+        $.get('https://api.shodan.io/shodan/host/search', {
                 key: API_KEY,
                 query: queryVal
             })
             .done(function (data) {
-                //console.log(data.matches);
-                var html = "";
-
+                var html = '';
                 data.matches.forEach(function (match) {
-                    html += (match.product || match.http.server) + "<br>";
+                    html += '<li class="list-group-item">'
+                    html +=     '<span class="badge">'
+                    html +=         '<i class="fa fa-check" aria-hidden="true"></i>'
+                    html +=     '</span>'
+                    html +=     (match.product || match.http.server);
+                    html += '</li>'
                 });
 
-                $("#results").html(html);
-
+                $('#results').append(html);
             });
     }
-
 });
